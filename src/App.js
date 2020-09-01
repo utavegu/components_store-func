748f933c2428ab7c3db3ac449bb192c78daf14c6
utavegu <utavegu@list.ru>
2020-09-01 22:07:20 +0500

    Мелкие правки и оставшийся ворнинг

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ShopItemFunc from './components/ShopItemFunc.jsx';
+import ItemModel from './models/ItemModel.js';
 
 function App() {
-  const item = {
+  const item = new ItemModel ({
     brand: 'Tiger of Sweden',
     title: 'Leonard coat',
     description: 'Minimalistic coat in cotton-blend',
     descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
     price: 399,
     currency: '£'
-  }
+  });
   
   return (
     <div className="container">
@@ -22,7 +23,7 @@ function App() {
         <ShopItemFunc item={item} />
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
